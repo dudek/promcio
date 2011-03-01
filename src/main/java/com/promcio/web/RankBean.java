413ac10cf81fe05cdca41b55bcd1ac3b4a3d8ba4
Keysha <karolina.anna.kruk@gmail.com>
2011-03-01 21:13:02 +0100

    Tworzenie Bean'a sesyjnego wraz z wszystkimi polami dla klasy Rank.

---
+++ src/main/java/com/promcio/web/RankBean.java
@@ -0,0 +1,36 @@
+package com.promcio.web;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped
+@Named
+public class RankBean  implements Serializable{
+
+	private static final long serialVersionUID = -6961978690202962358L;
+	
+	
+	private String name;
+
+	private float hourSalary;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setHourSalary(float hourSalary) {
+		this.hourSalary = hourSalary;
+	}
+
+	public float getHourSalary() {
+		return hourSalary;
+	}
+	
+
+}
