3b17ef7d8331d2b5acc3af167d1cfe80274c67c9
yahetai <yakehy-inf@yahoo.com>
2011-03-12 19:45:30 +0100

    Dodano CompanyManager wraz z metodami join dla Company, Employee i Rank

---
+++ src/main/java/com/promcio/service/CompanyManager.java
@@ -0,0 +1,47 @@
+package com.promcio.service;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import com.promcio.domain.Employee;
+import com.promcio.domain.Rank;
+
+@Stateless
+public class CompanyManager {
+	 
+	 @PersistenceContext
+	 EntityManager em;
+	 
+	 //TODO addCompany()
+	 
+	 //TODO removeCompany()
+	 
+	 public void joinCompanyWithEmployee(long companyId, long employeeId) {
+			Company company = em.find(Company.class, companyId);
+			Employee employee = em.find(Employee.class, employeeId);
+			
+			List<Employee> employees = company.getEmployees();
+			
+			employees.add(employee);
+			
+			company.setEmployees(employees);
+			employee.setCompany(company);
+			
+			em.persist(company);
+			em.persist(employee);
+	 }
+	 
+	 public void joinCompanyWithRank(long companyId, long rankId) {
+			Company company = em.find(Company.class, companyId);
+			Rank rank = em.find(Rank.class, rankId);
+			
+			List<Rank> ranks = company.getRanks();
+			
+			ranks.add(rank);
+			
+			company.setRanks(ranks);
+			
+			em.persist(company);
+	 }
+}
